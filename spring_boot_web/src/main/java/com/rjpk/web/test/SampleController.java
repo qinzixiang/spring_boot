2bd6fec1010107fa1dd878ab8af6b0df3bc775b5
zhinanzhen <xuxn163com@163.com>
2017-12-05 09:50:55 +0800

    init

---
+++ spring_boot_web/src/main/java/com/rjpk/web/test/SampleController.java
@@ -0,0 +1,27 @@
+package com.rjpk.web.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ClassName SampleController
+ * @Description
+ * @Author xiangnan.xu
+ * @DATE 2017/12/4 15:34
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
